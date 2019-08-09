43d0742ddaf7fb949eed2f3de22bd4c64ce87fbe
annisatahira <annisatahira>
2019-08-09 11:33:39 +0700

    jawaban task 4

--- app.js
+++ app.js
@@ -138,6 +138,23 @@ app.post("/task5", (req, res) => {
 
     return res.send(result)
 })
+
+
+app.get("/task1", (req, res) => {
+    let key1 = req.query.key1
+    let key2 = req.query.key2
+
+    let result = task_condition.task1(key1, key2)
+
+    return res.send(result)
+})
+
+app.post("/task2", (req, res) => {
+    let { name, email } = req.body
+    let result = task_condition.task2(name, email)
+
+    return res.send(result)
+})
 //
 
 /**
