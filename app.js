141e208dc38705d0e86ee5f091d859b855a4aee2
annisatahira <annisatahira>
2019-08-09 16:47:47 +0700

    add loop_map_object

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const task_condition = require("./lib/task-condition")
 const loop_for = require('./lib/loop_for')
 const loop_foreach = require('./lib/loop_foreach')
 const loop_map = require('./lib/loop_map')
+const map_object = require('./lib/loop_map_object')
 /**
  * List of imported routes
  */
@@ -176,6 +177,27 @@ app.get("/loop-foreach", (req, res) => {
 app.get("/loop-map", (req, res) => {
     let data = ["Red", "Blue", "Green"]
     let result = loop_map(data)
+
+    return res.send(result)
+})
+
+app.get('/object-loop', (req, res) => {
+    let data = [
+        {
+            name: "Red",
+            note: "Danger"
+        },
+        {
+            name: "Yellow",
+            note: "Warning"
+        },
+        {
+            name: "Green",
+            note: "Success"
+        }
+    ]
+
+    let result = map_object(data)
 
     return res.send(result)
 })
